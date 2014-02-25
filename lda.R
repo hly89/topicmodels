1a2346757de8922afd2fc5e94d9364f9a37982ff
hly89 <heliye89@gmail.com>
2014-02-25 18:32:44 +0200

    test data and training data done without deleting zero row

--- lda.R
+++ lda.R
@@ -1,4 +1,11 @@
+test<-dtm
+training<-dtm
+test$i<-dtm_test$i
+test$j<-dtm_test$j
+test$v<-dtm_test$v
+te<-dtm[row_sums(dtm)>0,];
 #LAD
 k<-30; # number of topics
 seed<-2000;
+
 gibbs<-LDA(dtm_training, k=k, method="Gibbs", control=list(seed=seed, burnin=1000, thin=100, iter=2000));
