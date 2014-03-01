6118a8f5887e75fcab2d84ddddb5e19a7ebd2e46
hly89 <heliye89@gmail.com>
2014-03-01 23:41:20 +0200

    perplexity done for lda topic number 5,10,15

--- 1.R
+++ 1.R
@@ -3,3 +3,18 @@ gibbs_lda<-list()
 for(gibbs_idx in 1:length(number_topic)){
   gibbs_lda[[gibbs_idx]]<-LDA(dtm_training, k=number_topic[gibbs_idx], method="Gibbs", control=list(seed=seed, burnin=1000, thin=100, iter=2000));
 }
+
+per_lda5<-vector("numeric",length(g))
+for(per_i in 1:length(g)){
+  per_lda5[per_i]<-perplexity(gibbs_lda[[1]],dtm_test[g[[per_i]],])
+}
+
+per_lda10<-vector("numeric",length(g))
+for(per_i in 1:length(g)){
+  per_lda10[per_i]<-perplexity(gibbs_lda[[2]],dtm_test[g[[per_i]],])
+}
+
+per_lda15<-vector("numeric",length(g))
+for(per_i in 1:length(g)){
+  per_lda15[per_i]<-perplexity(gibbs_lda[[3]],dtm_test[g[[per_i]],])
+}
